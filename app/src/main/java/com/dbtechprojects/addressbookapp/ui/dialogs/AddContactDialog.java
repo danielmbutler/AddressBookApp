afdab7f35a0ffbebaa8920a28537f70e244bbd35
Daniel Butler <danielmbutler@hotmail.co.uk>
2021-08-05 16:04:05 +0100

    creating layouts and adding dimens

---
+++ app/src/main/java/com/dbtechprojects/addressbookapp/ui/dialogs/AddContactDialog.java
@@ -0,0 +1,13 @@
+package com.dbtechprojects.addressbookapp.ui.dialogs;
+
+import android.app.Dialog;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+public class AddContactDialog extends Dialog {
+
+    public AddContactDialog(@NonNull Context context) {
+        super(context);
+    }
+}
